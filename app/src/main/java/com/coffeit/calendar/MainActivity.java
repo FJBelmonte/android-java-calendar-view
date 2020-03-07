de08448aa14f2f356821f51e12e56f60c0567322
Fernando Belmonte <fernando.belmonte@live.com>
2020-03-07 18:40:38 -0300

    Initial commit

---
+++ app/src/main/java/com/coffeit/calendar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.coffeit.calendar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private CalendarView calendarView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        calendarView.findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
+
+ */
+    }
+}
