4f924197379c41ef3d01882eac4f7e210f9a509a
Fernando Belmonte <fernando.belmonte@live.com>
2020-03-07 18:52:35 -0300

    final setup

--- app/src/main/java/com/coffeit/calendar/MainActivity.java
+++ app/src/main/java/com/coffeit/calendar/MainActivity.java
@@ -4,25 +4,61 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class MainActivity extends AppCompatActivity {
 
     //private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        calendarView.findViewById(R.id.calendarView);
+
+        calendarView = findViewById(R.id.calendarView);
+
+        /*calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2015,1,1))
+                .setMaximumDate(CalendarDay.from(2018,3,1))
+                .commit();*/
+        CharSequence months[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(months);
+
+        CharSequence weeks[] = {"Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"};
+        calendarView.setWeekDayLabels(weeks);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data: ", "valor: " + (date.getMonth() + 1) + "/" + date.getYear());
+            }
+        });
+
+        /*
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Log.i("data: ", "valor: " + date );
+            }
+        });*/
+
+        /*
+        calendarView = findViewById(R.id.calendarView);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-
+                Log.i("data: ", "valor: " + dayOfMonth + "/" + month + "/" + year );
             }
-        });
+        });*/
 
- */
     }
 }
